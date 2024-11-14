8bd4343e81e1bee06bc7b846965cecbb0c2b462c
rottensquid <calamaririce@gmail.com>
2024-11-14 01:06:06 +0000

    changes

--- src/app.js
+++ src/app.js
@@ -5,7 +5,21 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let pronoun = ["the", "our", "that", "we", "any"];
+let adj = ["great", "big", "afraid", "large"];
+let noun = ["jogger", "racoon", "cat", "wizard"];
+let extension = [".com", ".net", ".us", ".io"];
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let allDomains = "";
+  for (let p of pronoun) {
+    for (let a of adj) {
+      for (let n of noun) {
+        for (let e of extension) {
+          allDomains += p + a + n + e + "<br>";
+        }
+      }
+    }
+  }
+  document.querySelector("#domain-gen").innerHTML = allDomains;
 };
